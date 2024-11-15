447aba5f58c8e286218ced789959295fdd902010
ArtemOrlovUA <artemorel200415@gmail.com>
2024-11-15 14:47:09 +0200

    Add navigation link to home on ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function ErrorPage() {
   return (
     <div>
       <p>Вибачте, щось пішло не так</p>
+      <Link to="/home">На головну</Link>
     </div>
   );
 }
